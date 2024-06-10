3da92838a21950267eb6e5129893f99aa01d9f4f
SimiCow <simhikad@gmail.com>
2024-06-10 09:16:22 +0200

    Second Commit

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.weatherapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var exitButton = findViewById<Button>(R.id.exitAppBtn)
+        var enterButton = findViewById<Button>(R.id.mainScreenBtn)
+
+        exitButton.setOnClickListener{
+            finish()
+        }
+
+        enterButton.setOnClickListener{
+            val intent = Intent(this, MainScreen::class.java)
+        }
     }
+
 }
